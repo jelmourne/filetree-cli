21f71be5dba3a23e4451635688859af15689bfb0
Justin Elmourne <jelmourne@outlook.com>
2024-03-14 15:23:22 -0400

    Moved to struct

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,83 @@
 use std::{env, fs::read_dir, path::PathBuf};
 
-fn main() {
-    show_tree();
+#[derive(Clone)]
+struct Directory {
+    file: PathBuf,
+    children: Vec<Directory>
 }
 
-fn represent_tree(dir: PathBuf) -> Vec<String> {
-    let mut files = Vec::new();
-    for entity in read_dir(dir).expect("Error reading dir") {
+impl Directory {
+    fn print_tree(self) {
+        println!("{}/",format!("{:?}",env::current_dir()
+            .unwrap()
+            .file_name()
+            .expect("Dir"))
+            .replace("\"", ""));
+
+        for entry in represent_string_tree(self) {
+            println!("{}",entry)
+        }
+    }
+}
+
+
+fn main() {
+    let tree = build_tree(env::current_dir().unwrap());
+    tree.print_tree()
+    
+}
+
+fn build_tree(path:PathBuf) -> Directory {
+    let mut dir = Directory {
+        file: path.clone(),
+        children: Vec::new()
+    };
+    for entity in read_dir(path).expect("Could not read directory") {
         match entity {
             Ok(entity)=> {
-                let file = if entity.path().is_dir() {format!("|- {:?}/", entity.file_name())} else {format!("|- {:?}", entity.file_name())};
-                files.push(file.replace("\"", ""));
-
-                if entity.path().is_dir(){
-                    let temp_tree:Vec<String> =  represent_tree(entity.path())
-                        .into_iter()
-                        .map(|s|format!("|   {}", s))
-                        .collect();
-
-                    files = [&files[..], &temp_tree]
-                        .concat();
+                if entity.path().is_dir() {
+                    dir.children.push(build_tree(entity.path()));
+                    continue;
                 }
-                
-
+                dir.children.push(Directory {
+                    file: entity.path(),
+                    children: Vec::new()
+                })
             }
             Err(error) => {
-                println!("{}",error)
+                println!("{}", error)
             }
         }
+    }
+    dir
+}
+
+
+fn format_file(file: &PathBuf) -> String {
+    let formated_file = 
+        if file.is_dir() {format!("|- {}/", file.file_name().unwrap().to_string_lossy())} 
+        else {format!("|- {}", file.file_name().unwrap().to_string_lossy())};
+
+    formated_file.replace("\"", "")
+}
+
+
+fn represent_string_tree(dir: Directory) -> Vec<String> {
+    let mut files = Vec::new();
+    for entity in dir.children {
+            files.push(format!("{}",format_file(&entity.file)));
+
+            if !entity.children.is_empty(){
+                let temp_tree:Vec<String> =  represent_string_tree(entity)
+                    .into_iter()
+                    .map(|s|format!("|  {}", s))
+                    .collect();
+
+                files = [&files[..], &temp_tree]
+                    .concat();
+            }    
     }
     return files;
 }
 
-fn show_tree() {
-    for entry in represent_tree(env::current_dir().unwrap()) {
-        println!("{}",entry)
-    }
-}
+
